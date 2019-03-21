ff6b635aeba5dafffe541aedee345186185889d4
smallnancy <smallnancy@outlook.com>
2019-03-21 17:33:20 +0800

    mybatis log4j

--- people/src/cn/nancy/test/Test.java
+++ people/src/cn/nancy/test/Test.java
@@ -8,18 +8,16 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.log4j.Logger;
 
 import cn.nancy.pojo.People;
 
 public class Test {
   public static void main(String[] args) throws IOException {
-		InputStream is = Resources.getResourceAsStream("mybatis.xml");
-		SqlSessionFactory fac = new SqlSessionFactoryBuilder().build(is);
-		SqlSession session = fac.openSession();
-		List<People> list = session.selectList("cn.nancy.mapper.PeopleMapper.selectAll");
-		for(People p: list) {
-			System.out.println(p.toString());
-		}
-	  session.close();
+  	InputStream is = Resources.getResourceAsStream("mybatis.xml");
+  	SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is);
+  	SqlSession session = factory.openSession();
+  	List<People> list =  session.selectList("cn.nancy.pojo.People.selectAll");
+  	session.close();
 	}
 }
